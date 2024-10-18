0df8be1c92da65d83d23e7f3bd4740ba3bd8ac1a
Amrita <amritak.saw@gmail.com>
2024-10-18 13:57:31 -0400

    stylistic elements on page

---
+++ index.js
@@ -0,0 +1,20 @@
+// limit to one 3 fortunes per day - click tracking
+// localStorage - you can save stuff in a window for later 
+// store when were the last clicks made date and time
+
+// after 3 clicks on the page, any button pressed should display an angry popup
+    //I'm an octopus, not a fortune factory!
+// offer octupus fun facts second time use tries to surpass fortune limit
+//finding an API for fortunes or octupus facts - don't use anything with an API Key yet
+// ideal to use something that exists rather than make it
+
+// a "DO NOT TOUCH ME THERE" button
+//issues are figuiring out where the suction pads are
+
+// mouseover animations - last priority
+// should be pretty striaghtforward - regular octopus to angry
+// key frames - can be used for shaking or something
+// "no returns or refunds!!" // "I cannot guarantee results"
+
+//do you have any questions. comments, or concerns for this ocutopus
+//can event listeners trigger audio? download audio file and put it in assets and then research
